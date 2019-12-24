ac2c03827dcdd211b71189c1a6a87e854425f170
Kirill <58871012+buhaistrikalo@users.noreply.github.com>
2019-12-24 17:44:53 +0700

    Страница 111
    Изучил/повторил:
    React:
    – использование this.state и setState() для управления внутренним состоянием
    компонента
    – передача функций или методов класса элементу обработчика
    – использование форм и событий в React для добавления взаимодействий
    – однонаправленный поток данных — важная концепция в React
    – постигли контролируемые компоненты
    – составлять компоненты с дочерними компонентами и повторно используемыми
    компонентами
    – использование и реализация классовых компонентов и функциональных компонентов без состояния
    – подходы к стилизации компонентов
    ES6:
    – функции, связанные с классом — это методы класса
    – деструктуризация объектов и массивов
    – параметры по умолчанию
    – функции высшего порядка

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var hello = 'Добро пожаловать, снова';
+
+const User = [
+  {
+    fname: 'Kirill',
+    lname: 'Podyachenko',
+    age: 21,
+    objectID: 0,
+  },
+  {
+    fname: 'Alexandra',
+    lname: 'Masyk',
+    age: 19,
+    objectID: 1,
+  }
+];
+
+const isSearched = searchTerm => item => (
+  item.fname.toLowerCase().includes(searchTerm.toLowerCase()) //Функция поиска
+)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      User,
+      searchTerm: '',
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updateUser = this.state.User.filter(isNotId);    
+    this.setState({ User: updateUser });
+  };
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  
+
+  render() {
+    const { searchTerm, User } = this.state;
+    return (
+      <div className="page">
+        <h2> { hello }</h2>
+        <div className='interactions'>
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Поиск
+          </Search>
+        </div>
+        <Table 
+          User={User}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children}: <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const largeColumn = {
+  width: '40%',
+  };
+  const midColumn = {
+  width: '30%',
+  };
+  const smallColumn = {
+  width: '10%',
+  };
+  
+
+const Table = ({ User, pattern, onDismiss }) =>
+  <div className='table'>
+    {User.filter(isSearched(pattern)).map(item => 
+      <div key={item.objectID} className="table-row">
+        <span style={ midColumn }>{item.fname} </span>
+        <span style={ midColumn }>{item.lname} </span>
+        <span style={ smallColumn }>{item.age}</span>
+        <span> 
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
+            Удалить
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className = {className}
+    type = 'button'
+  >
+    {children}
+  </button>
