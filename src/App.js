8489214bd6617c64143d63c3713dd181f3943be1
Kirill <58871012+buhaistrikalo@users.noreply.github.com>
2019-12-27 20:00:34 +0700

    Кнопка "Показать больше историй"
    На сайте очень много статей. Но нельзя показывать их все сразу. Это просто не нужно.
    Сладовательно нужно сделать "страницы 1, 2,3..." по которым будет скролить пользователь, если ему это будет нужно.
    Число элементов: 10.
    Каждый клик по кнопке "Показать больше историй" добавляет ещё 10.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const DEFAULT_QUERY = 'kirill';
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '10';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
 
 var hello = 'Добро пожаловать, снова';
 
@@ -39,8 +43,8 @@ class App extends Component {
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
   }
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)    
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
@@ -53,7 +57,17 @@ class App extends Component {
   }
     
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0
+    ? this.state.result.hits
+    : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
   componentDidMount() {
     const { searchTerm } = this.state;
@@ -75,6 +89,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <h2> { hello }</h2>
@@ -93,6 +108,12 @@ class App extends Component {
             onDismiss={this.onDismiss}
           />
         }
+        <div className='interactions'>
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            Показать больше историй
+          </Button>
+        </div>
+
       </div>
     );
   }
