2aab19c399a3290f91afda29a0524e53bf5e6749
Kirill <58871012+buhaistrikalo@users.noreply.github.com>
2019-12-27 19:48:21 +0700

    Добавлен поиск
    Поиск со стороны клиента.
    Так же возникла проблема: некоторые элементы возращались без заголовка. А потом была успешно устронена фильтром.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const DEFAULT_QUERY = 'redux';
+const DEFAULT_QUERY = 'kirill';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
-console.log(url);
 
 var hello = 'Добро пожаловать, снова';
 
@@ -26,10 +25,6 @@ const User = [
   }
 ];
 
-/*const isSearched = searchTerm => item => (
-  item.fname.toLowerCase().includes(searchTerm.toLowerCase()) //Функция поиска
-)*/
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -41,16 +36,28 @@ class App extends Component {
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
   }
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
+  }
+    
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+    
   setSearchTopStories(result) {
     this.setState({ result });
   }
   componentDidMount() {
     const { searchTerm } = this.state;
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-    .then(response => response.json())
-    .then(result => this.setSearchTopStories(result))
-    .catch(error => error);
+    this.fetchSearchTopStories(searchTerm);
   }
     
   onDismiss(id) {
@@ -64,12 +71,10 @@ class App extends Component {
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
-
   
 
   render() {
     const { searchTerm, result } = this.state;
-    if (!result) { return null; }
     return (
       <div className="page">
         <h2> { hello }</h2>
@@ -77,15 +82,17 @@ class App extends Component {
           <Search 
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Поиск
           </Search>
         </div>
-        <Table 
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table 
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
@@ -93,29 +100,33 @@ class App extends Component {
 
 export default App;
 
-const Search = ({ value, onChange, children }) =>
-  <form>
+const Search = ({ value, onChange, onSubmit, children }) =>
+  <form onSubmit={onSubmit}>
     {children}: <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type='submit'>
+      {children}
+    </button>
   </form>
 
 const largeColumn = {
-  width: '40%',
+    width: '40%',
   };
   const midColumn = {
-  width: '30%',
+    width: '30%',
   };
   const smallColumn = {
-  width: '10%',
+    width: '10%',
   };
   
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className='table'>
-    {list.map(item => 
+    {list.filter(item => item.title > ' ').map(item => 
+      
       <div key={item.objectID} className="table-row">
         <span style={ largeColumn }>{item.title} </span>
         <span style={ midColumn }>{item.author} </span>
@@ -128,6 +139,7 @@ const Table = ({ list, pattern, onDismiss }) =>
       </div>
     )}
   </div>
+
 
 const Button = ({ onClick, className = '', children }) =>
   <button
