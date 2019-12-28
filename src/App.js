47a7d4a91eb22972f79c099e16e4e7773c77b156
Kirill <58871012+buhaistrikalo@users.noreply.github.com>
2019-12-28 23:55:29 +0700

    Axios + Mount
    Axios. Подключена новая библиотека для работы с API.
    Mount. Отслеживание размонтирования компонентов

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -10,6 +11,7 @@ const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
+
 
 
 var hello = 'Добро пожаловать, снова';
@@ -30,6 +32,7 @@ const User = [
 ];
 
 class App extends Component {
+  _isMounted = false;
   constructor(props) {
     super(props);
 
@@ -52,10 +55,9 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)    
-    .then(response => response.json())
-    .then(result => this.setSearchTopStories(result))
-    .catch(error => this.setState({ error }));
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(result => this._isMounted && this.setSearchTopStories(result.data))
+    .catch(error => this._isMounted && this.setState({ error }));
   }
     
   onSearchSubmit(event) {
@@ -87,10 +89,15 @@ class App extends Component {
     });
   }
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+    }    
     
   onDismiss(id) {
     const { searchKey, results } = this.state;
