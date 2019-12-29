eb1ecb348ada5e00d40a1af90a4e057c14db45c9
Kirill <58871012+buhaistrikalo@users.noreply.github.com>
2019-12-29 12:32:37 +0700

    Тестирование отдельно взятых компонентов

--- src/App.js
+++ src/App.js
@@ -154,3 +154,5 @@ class App extends Component {
 }
 
 export default App;
+
+export { Button, Search, Table };
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+import App, {Search, Button, Table} from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Search', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Поиск</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+    test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Search>Поиск</Search>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+    
 });
